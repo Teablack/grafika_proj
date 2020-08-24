532faec2fb4b355f172681ee656364bccc4ac224
Teablack <34287403+Teablack@users.noreply.github.com>
2020-08-24 16:57:52 +0200

    Update f_lamberttextured.glsl

--- f_lamberttextured.glsl
+++ f_lamberttextured.glsl
@@ -1,18 +1,4 @@
-/*
-#version 330
-uniform sampler2D tex;
 
-out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
-in vec2 i_tc;
-in vec4 nl;
-in float rv;
-
-void main(void) {
-	
-	vec4 color=texture(tex,i_tc);
-	pixelColor= vec4(color.rgb*nl,color.a)+vec4(rv,rv,rv,0);
-}
-*/
 #version 330
 
 uniform sampler2D tex;
@@ -23,7 +9,7 @@ in float rv;
 in vec2 cord;
 
 void main(void) {
-	
-	pixelColor = vec4(nl * texture(tex,cord).rgb,1.0f)+vec4(rv,rv,rv,0); //Wyliczenie modelu oświetlenia (bez ambient)
-
+	float ambient = 0.1f;
+	pixelColor = vec4(ambient*texture(tex,cord).rgb,1.0f)	+	vec4(nl * texture(tex,cord).rgb,1.0f)		+	vec4(rv,rv,rv,0);
+	//                       tu ambient									 //diffuse                            //specular
 }
