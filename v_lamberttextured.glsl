f9acde8fa7e3e4b669f586e6ca66508768e7d9d7
Teablack <34287403+Teablack@users.noreply.github.com>
2020-09-17 14:31:52 +0200

    Update v_lamberttextured.glsl

--- v_lamberttextured.glsl
+++ v_lamberttextured.glsl
@@ -1,32 +1,27 @@
+#version 330 core
 
+layout (location = 0) in vec4 vertex;
+layout (location = 1) in vec2 texCoord;
+layout (location = 2) in vec3 normal;
 
-#version 330
-
-//Zmienne jednorodne
-uniform mat4 P;
-uniform mat4 V;
 uniform mat4 M;
-uniform vec4 lp;
+uniform mat4 V;
+uniform mat4 P;
+uniform vec4 lp; //pozycja dla swiatla punktowego
+uniform vec3 vp;
 
-//Atrybuty
-in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
-in vec2 texCoord; //kolor wierzchołka
-in vec4 normal; //wektor normalny wierzchołka w przestrzeni modelu
+out vec2 i_tc;
+out vec3 n;
+out vec4 FragPos;
+out vec4 lightPos;
+out vec3 viewPos;
 
-out float nl;
-out float rv;
-out vec2 cord;
-
-void main(void) {
-    
-    vec4 l = normalize(V * (lp - M * vertex));//znormalizowany wektor do światła w przestrzeni oka
-    vec4 n = normalize(V * M * normal);//znormalizowany wektor normalny w przestrzeni oka
-    vec4 r = reflect(-l, n); //Wektor odbity
-    vec4 v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //Wektor do obserwatora w przestrzeni oka
-
-    nl = clamp(dot(n, l),0,1); //Kosinus kąta pomiędzy wektorami n i l.
-    rv = pow(clamp(dot(r, v), 0, 1), 256); // Kosinus kąta pomiędzy wektorami r i v podniesiony do 25 potęgi
-    cord=texCoord;
-    
-    gl_Position=P*V*M*vertex;
-}
+void main()
+{
+    viewPos = vp;
+    lightPos = lp;
+    n = normal;
+    i_tc= texCoord;
+    FragPos = M* vertex;
+    gl_Position = P*V*M *vertex;
+} 
